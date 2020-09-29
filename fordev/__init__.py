0d5e557f8dc8fc660a598ffcd8cb6d5eead408e3
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-09-29 19:06:09 -0300

    Create docstring of fordev module

--- fordev/__init__.py
+++ fordev/__init__.py
@@ -1,4 +1,44 @@
 # -*- coding: utf-8 -*-
+"""
+The Fordev module is a generator, validator and manipulator of random data.
+
+Basic use:
+
+```
+>>> from fordev.generator import people
+>>> p = people(sex='M', age=25, state='SP')
+>>> print(p)
+
+# Output ---------------------------------------------------------------------------------
+{
+    'altura': '1,90',
+    'bairro': 'Jardim Maria Amélia',
+    'celular': '(12) 98401-5301',
+    'cep': '12318-110',
+    'cidade': 'Jacareí',
+    'cor': 'laranja',
+    'cpf': '061.632.758-70',
+    'data_nasc': '06/12/1995',
+    'email': 'bentoyagolorenzogoncalves-72@alcastro.com.br',
+    'endereco': 'Rua José Benedito de Oliveira',
+    'estado': 'SP',
+    'idade': 25,
+    'mae': 'Tereza Melissa Priscila',
+    'nome': 'Bento Yago Lorenzo Gonçalves',
+    'numero': 760,
+    'pai': 'Sérgio Guilherme Erick Gonçalves',
+    'peso': 88,
+    'rg': '23.920.314-8',
+    'senha': 'ErOKUUyoml',
+    'sexo': 'Masculino',
+    'signo': 'Sagitário',
+    'telefone_fixo': '(12) 2844-9806',
+    'tipo_sanguineo': 'AB+'
+}
+```
+
+More info at: https://github.com/matheusfelipeog/fordev
+"""
 
 
 __all__ = [

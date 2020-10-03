deb25ce947ea4e4bb5b36d6818734b9325f0a1c0
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-03 17:44:55 -0300

    Join project information into one file

--- fordev/__about__.py
+++ fordev/__about__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+"""This are info about project."""
 
+__version__ = '0.1.0'
 __author__ = 'Matheus Felipe'
 __email__ = 'matheusfelipeog@protonmail.com'
 __github__ = 'https://github.com/matheusfelipeog'

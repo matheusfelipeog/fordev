8d230f543c5fa4dab0c7e7bd6db706f930fe14a6
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-05 22:15:04 -0300

    Implemented verify cnh function

--- fordev/validator.py
+++ fordev/validator.py
@@ -9,3 +9,35 @@ from .__about__ import __github__
 
 __version__ = __version__
 __author__ = f'{__author__} <{__email__}> and <{__github__}>'
+
+# --- Local libraries ---
+from ._base import fordev_request
+
+
+def cnh(cnh_code: str, data_only: bool=True) -> bool:
+    """Check if CNH code is valid.
+    
+    Keyword arguments:
+
+    `cnh_code: str` - CNH code for check.
+    
+    `data_only: bool` - If True, return data only. If False, return msg and data/error.
+    """
+
+    content_length = 36
+    referer = 'validador_cnh'
+    payload = {
+        'acao': 'validar_cnh',
+        'txt_cnh': cnh_code
+    }
+
+    r = fordev_request(content_length, referer, payload)
+
+    # Check if is valid and replace data
+    is_valid = r['data'].split(' - ')[-1].lower() == 'verdadeiro'
+    r['data'] = is_valid
+
+    if data_only and r['msg'] == 'success':
+        return r['data']
+
+    return r

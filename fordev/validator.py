e0ab3f487b20bbee8aa06bc14ac2399b40bd98fc
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-05 21:35:12 -0300

    Add info about the  project

--- fordev/validator.py
+++ fordev/validator.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 """Module for validating data."""
+
+from .__about__ import __version__
+
+from .__about__ import __author__
+from .__about__ import __email__
+from .__about__ import __github__
+
+__version__ = __version__
+__author__ = f'{__author__} <{__email__}> and <{__github__}>'

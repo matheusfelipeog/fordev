66d50db7c108a9947f87274337e2e4c2e47f3cf5
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-22 18:59:04 -0300

    Update version for add wheel structure in pypi

--- fordev/__about__.py
+++ fordev/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """This are info about project."""
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Matheus Felipe'
 __email__ = 'matheusfelipeog@protonmail.com'
 __github__ = 'https://github.com/matheusfelipeog'

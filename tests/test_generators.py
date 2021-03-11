4b6652f4235f35a25ac810c22c3a710bf818014d
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-10 23:56:25 -0300

    Create base structure of generators tests

---
+++ tests/test_generators.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Tests of fordev.generators module."""
+
+import unittest
+
+from fordev.generators import vehicle_brand
+from fordev.generators import uf
+
+
+class TestGenerators(unittest.TestCase):
+    def test_vehicle_brand_generator(self):
+        ...
+
+if __name__ == '__main__':
+    unittest.main()

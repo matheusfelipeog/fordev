4a71599318d9e9c72f2e76c70aaf15f765fbb129
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-12 23:06:49 -0300

    Add test for min, max and exceed number of vehicle brands generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -19,6 +19,18 @@ class TestGenerators(unittest.TestCase):
         self.assertIsInstance(result['msg'], str)
         self.assertIsInstance(result['data'], list)
 
+    def test_if_vehicle_brand_generator_returns_max_and_min_number_of_data(self):
+        n_min = vehicle_brand(n=1)
+        n_max = vehicle_brand(n=87)
+        self.assertGreaterEqual(len(n_min), 1)
+        self.assertLessEqual(len(n_max), 87)
+    
+    def test_if_vehicle_brand_generator_not_exceed_min_and_max_limit_of_return(self):
+        with self.assertRaises(ValueError):
+            vehicle_brand(n=0)
+            vehicle_brand(n=-1)  
+            vehicle_brand(n=88)
+
 
 if __name__ == '__main__':
     unittest.main()

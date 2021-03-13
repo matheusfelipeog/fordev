8605179a3461d541e8ea09efe804c8b30a101430
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-13 00:15:26 -0300

    Add tests of uf generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -38,6 +38,34 @@ class TestGenerators(unittest.TestCase):
         for brand in brands:
             self.assertIsInstance(brand, str)
 
+    def test_uf_generator_with_data_only_argument_as_true(self):
+        ufs = uf(data_only=True)
+        self.assertIsInstance(ufs, list)
+    
+    def test_uf_generator_with_data_only_argument_as_false(self):
+        ufs = uf(data_only=False)
+        self.assertIsInstance(ufs, dict)
+        self.assertCountEqual(['msg', 'data'], ufs.keys())
+        self.assertEqual(ufs['msg'], 'success')
+        self.assertIsInstance(ufs['data'], list)
+
+    def test_if_uf_generator_returns_max_and_min_number_of_data(self):
+        min_ufs = uf(n=1)
+        max_ufs = uf(n=27)
+        self.assertGreaterEqual(len(min_ufs), 1)
+        self.assertLessEqual(len(max_ufs), 27)
+    
+    def test_if_uf_generator_not_exceed_min_and_max_limit_of_return(self):
+        with self.assertRaises(ValueError):
+            uf(n=0)
+            uf(n=-1)  
+            uf(n=28)
+
+    def test_if_data_type_of_uf_generator_return_is_string(self):
+        ufs = uf(n=27)
+        for uf_code in ufs:
+            self.assertIsInstance(uf_code, str)
+
 
 if __name__ == '__main__':
     unittest.main()

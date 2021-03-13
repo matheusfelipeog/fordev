c5fe55b1572a4f959cc7580f1b91ec0d8f49b366
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-13 00:33:13 -0300

    Add test to raise for invalid uf

---
+++ tests/test_validators.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Tests of fordev.validators module."""
+
+import unittest
+
+from fordev.validators import raise_for_invalid_uf
+
+
+class TestValidators(unittest.TestCase):
+    """Test Class of fordev.validators module."""
+    
+    def test_raise_for_invalid_uf(self):
+        raise_for_invalid_uf('SP')
+        raise_for_invalid_uf('', include_blank=True)
+        with self.assertRaises(ValueError):
+            raise_for_invalid_uf('HUE')
+            raise_for_invalid_uf('', include_blank=False)
+
+
+if __name__ == '__main__':
+    unittest.main()

7b08bf09d0db202853656b81a90887131cf1a29a
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-12 22:26:42 -0300

    Add tests of vehicle brand generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -8,8 +8,17 @@ from fordev.generators import uf
 
 
 class TestGenerators(unittest.TestCase):
-    def test_vehicle_brand_generator(self):
-        ...
+    def test_vehicle_brand_generator_with_data_only_argument_as_true(self):
+        result = vehicle_brand(data_only=True)
+        self.assertIsInstance(result, list)
+    
+    def test_vehicle_brand_generator_with_data_only_argument_as_false(self):
+        result = vehicle_brand(data_only=False)
+        self.assertIsInstance(result, dict)
+        self.assertCountEqual(['msg', 'data'], result.keys())
+        self.assertIsInstance(result['msg'], str)
+        self.assertIsInstance(result['data'], list)
+
 
 if __name__ == '__main__':
     unittest.main()

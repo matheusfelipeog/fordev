6bb22388ec635db594433bad2d65d18ed9c7589b
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-16 16:48:57 -0300

    Fix parameters title in docstring

--- fordev/filters.py
+++ fordev/filters.py
@@ -38,8 +38,8 @@ def data_format(data_only: bool, data_dict: dict) -> dict:
 def filter_bank_account_info(html: str) -> dict:
     """Filtra dados de conta bancária contidos na estrutura HTML.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     html
         Uma estrutra HTML contendo dados de conta bancária
         retornados pela API do site 4devs.
@@ -63,8 +63,8 @@ def filter_bank_account_info(html: str) -> dict:
 def filter_vehicle_info(html: str) -> dict:
     """Filtra dados de veículo contido na estrutura HTML.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     html
         Uma estrutra HTML contendo dados de veículo
         retornados pela API do site 4devs.
@@ -88,8 +88,8 @@ def filter_vehicle_info(html: str) -> dict:
 def filter_credit_card_info(html: str) -> dict:
     """Filtra dados de cartão de crédito contidos na estrutura HTML.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     html
         Uma estrutra HTML contendo dados de cartão de crédito
         retornados pela API do site 4devs.
@@ -113,8 +113,8 @@ def filter_credit_card_info(html: str) -> dict:
 def filter_company_info(html: str) -> dict:
     """Filtra dados de companhia (empresa/organização) contidos na estrutura HTML.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     html
         Uma estrutra HTML contendo dados de companhia (empresa/organização)
         retornados pela API do site 4devs.
@@ -138,8 +138,8 @@ def filter_company_info(html: str) -> dict:
 def filter_city_name(html: str) -> list:
     """Filtra dados de cidade contidos na estrutura HTML.
 
-    Parameter
-    ---------
+    Parameters
+    ----------
     html
         Uma estrutra HTML contendo dados de uma cidade
         retornados pela API do site 4devs.

5881a41573f1ffd688d3bcc445e15f25a4654734
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2021-03-16 21:15:31 -0300

    Update version of 0.2.2 to 1.0.0

--- fordev/__about__.py
+++ fordev/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Metadados do projeto."""
 
-__version__ = '0.2.2'
+__version__ = '1.0.0'
 __author__ = 'Matheus Felipe'
 __email__ = 'matheusfelipeog@protonmail.com'
 __author_github__ = 'https://github.com/matheusfelipeog'

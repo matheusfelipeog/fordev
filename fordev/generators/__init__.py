5a53bacb94f12b439f996b762dc13384229989e7
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-11-05 17:26:50 -0300

    Transfer cpf function to generators package

--- fordev/generators/__init__.py
+++ fordev/generators/__init__.py
@@ -6,3 +6,4 @@ fordev.generators
 from fordev.generators.certificate import certificate
 from fordev.generators.cnh import cnh
 from fordev.generators.bank_account import bank_account
+from fordev.generators.cpf import cpf

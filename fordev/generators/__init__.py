4429c6e4e1ea015b906956d10832542c87d7a129
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-11-05 18:17:38 -0300

    Transfer people function to generators package

--- fordev/generators/__init__.py
+++ fordev/generators/__init__.py
@@ -15,3 +15,4 @@ from fordev.generators.rg import rg
 from fordev.generators.state_registration import state_registration
 from fordev.generators.voter_title import voter_title
 from fordev.generators.credit_card import credit_card
+from fordev.generators.people import people

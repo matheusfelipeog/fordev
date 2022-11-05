a60091a2977abe964538d22637e4fa4042d26bb6
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-11-05 17:48:21 -0300

    Transfer vehicle, vehicle_brand and vehicle_plate function to generators package

--- fordev/generators/__init__.py
+++ fordev/generators/__init__.py
@@ -9,3 +9,4 @@ from fordev.generators.bank_account import bank_account
 from fordev.generators.cpf import cpf
 from fordev.generators.pis_pasep import pis_pasep
 from fordev.generators.renavam import renavam
+from fordev.generators.vehicle import vehicle, vehicle_brand, vehicle_plate

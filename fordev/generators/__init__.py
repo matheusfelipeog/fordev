7bfb5431e95fca11b3c228b314b45d26ea5a751c
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-11-05 18:08:59 -0300

    Transfer voter_title function to generators package

--- fordev/generators/__init__.py
+++ fordev/generators/__init__.py
@@ -13,3 +13,4 @@ from fordev.generators.vehicle import vehicle, vehicle_brand, vehicle_plate
 from fordev.generators.cnpj import cnpj
 from fordev.generators.rg import rg
 from fordev.generators.state_registration import state_registration
+from fordev.generators.voter_title import voter_title

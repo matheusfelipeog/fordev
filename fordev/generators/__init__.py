99b45d103b7c0a5bc684e908611b53b2236ab207
Matheus Felipe <matheusfelipeog@protonmail.com>
2022-11-05 17:31:53 -0300

    Transfer pis_pasep function to generators package

--- fordev/generators/__init__.py
+++ fordev/generators/__init__.py
@@ -7,3 +7,4 @@ from fordev.generators.certificate import certificate
 from fordev.generators.cnh import cnh
 from fordev.generators.bank_account import bank_account
 from fordev.generators.cpf import cpf
+from fordev.generators.pis_pasep import pis_pasep
